cd2f6ac16418bfc5a6726fb6b5bd369b80b61338
David Custodio <jose.custodio@udgvirtual.udg.mx>
2023-10-18 21:39:05 -0600

    bossprint3d 0.1

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <p>Aquí va el Menú</p>
+      <div class="titleContainer">
+      <h1>Soportes de Echo Dot 3</h1> 
+    </div>
+    <section class="articleContainer">
+        <article>
+          <p>Aquí va el Catálogo</p>
+         </article>
+    </section>
+    
       </header>
     </div>
   );
