a64cb2526e5b8f83dfc5b12f652535212a3c87b3
David Custodio <jose.custodio@udgvirtual.udg.mx>
2023-10-24 19:32:38 -0600

    se agregan paginas terminadas y en proceso

--- src/ed5.js
+++ src/ed5.js
@@ -4,21 +4,22 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      <p>Aquí va el Menú</p>
-      <div class="titleContainer">
-      <h1>Soportes de Echo Dot 3</h1> 
+    <header>
+        <p>Aquí va el Menú</p>
+      </header>
+    
+    <div class="titleContainer">
+      <h1>Soportes de Echo Dot 5</h1> 
     </div>
+
     <section class="articleContainer">
         <article>
           <p>Aquí va el Catálogo</p>
-         </article>
+        </article>
     </section>
-    
-     </header>
+
     </div>
   );
 }
 
 export default App;
-
