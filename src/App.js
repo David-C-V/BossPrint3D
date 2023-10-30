5ad2e2ff1fd0cf4c981b702fa0723fdba5ed6a22
David Custodio <jose.custodio@udgvirtual.udg.mx>
2023-10-29 22:16:54 -0600

    Paginas perminadas y en proceso la ultima tarea

--- src/App.js
+++ src/App.js
@@ -14,7 +14,170 @@ function App() {
           <p>Aquí va el Catálogo</p>
          </article>
     </section>
-    
+    <div class="contenedor">
+
+              <div>
+                <h5>Articulo 1</h5>
+                <h6>Bogotá Colombia.Diciembre del 2022</h6>
+
+                <img src= "https://previews.123rf.com/images/aprillrain/aprillrain2212/aprillrain221200638/196354278-imagen-de-caricatura-de-un-astronauta-sentado-en-una-luna-ilustraci%C3%B3n-de-alta-calidad.jpg" width="50%"  ></img>
+               
+                <h5>Articulo 2</h5>
+                <h6>Bogotá Colombia.Diciembre del 2022</h6>
+
+                <img src= "https://previews.123rf.com/images/aprillrain/aprillrain2212/aprillrain221200638/196354278-imagen-de-caricatura-de-un-astronauta-sentado-en-una-luna-ilustraci%C3%B3n-de-alta-calidad.jpg" width="50%"  ></img>
+               
+                <h5>Articulo 3</h5>
+                <h6>Bogotá Colombia.Diciembre del 2022</h6>
+
+                <img src= "https://previews.123rf.com/images/aprillrain/aprillrain2212/aprillrain221200638/196354278-imagen-de-caricatura-de-un-astronauta-sentado-en-una-luna-ilustraci%C3%B3n-de-alta-calidad.jpg" width="50%"  ></img>
+               
+                <h5>Articulo 4</h5>
+                <h6>Bogotá Colombia.Diciembre del 2022</h6>
+
+                <img src= "https://previews.123rf.com/images/aprillrain/aprillrain2212/aprillrain221200638/196354278-imagen-de-caricatura-de-un-astronauta-sentado-en-una-luna-ilustraci%C3%B3n-de-alta-calidad.jpg" width="50%"  ></img>
+               
+                <h5>Articulo 5</h5>
+                <h6>Bogotá Colombia.Diciembre del 2022</h6>
+
+                <img src= "https://previews.123rf.com/images/aprillrain/aprillrain2212/aprillrain221200638/196354278-imagen-de-caricatura-de-un-astronauta-sentado-en-una-luna-ilustraci%C3%B3n-de-alta-calidad.jpg" width="50%"  ></img>
+               
+                <h5>Articulo 6</h5>
+                <h6>Bogotá Colombia.Diciembre del 2022</h6>
+
+                <img src= "https://previews.123rf.com/images/aprillrain/aprillrain2212/aprillrain221200638/196354278-imagen-de-caricatura-de-un-astronauta-sentado-en-una-luna-ilustraci%C3%B3n-de-alta-calidad.jpg" width="50%"  ></img>
+               
+                <h5>Articulo 7</h5>
+                <h6>Bogotá Colombia.Diciembre del 2022</h6>
+
+                <img src= "https://previews.123rf.com/images/aprillrain/aprillrain2212/aprillrain221200638/196354278-imagen-de-caricatura-de-un-astronauta-sentado-en-una-luna-ilustraci%C3%B3n-de-alta-calidad.jpg" width="50%"  ></img>
+               
+                <h5>Articulo 8</h5>
+                <h6>Bogotá Colombia.Diciembre del 2022</h6>
+
+                <img src= "https://previews.123rf.com/images/aprillrain/aprillrain2212/aprillrain221200638/196354278-imagen-de-caricatura-de-un-astronauta-sentado-en-una-luna-ilustraci%C3%B3n-de-alta-calidad.jpg" width="50%"  ></img>
+               
+                <h5>Articulo 9</h5>
+                <h6>Bogotá Colombia.Diciembre del 2022</h6>
+
+                <img src= "https://previews.123rf.com/images/aprillrain/aprillrain2212/aprillrain221200638/196354278-imagen-de-caricatura-de-un-astronauta-sentado-en-una-luna-ilustraci%C3%B3n-de-alta-calidad.jpg" width="50%"  ></img>
+               
+                <h5>Articulo 10</h5>
+                <h6>Bogotá Colombia.Diciembre del 2022</h6>
+
+                <img src= "https://previews.123rf.com/images/aprillrain/aprillrain2212/aprillrain221200638/196354278-imagen-de-caricatura-de-un-astronauta-sentado-en-una-luna-ilustraci%C3%B3n-de-alta-calidad.jpg" width="50%"  ></img>
+               
+
+              </div>
+
+              <div>
+              <h5>Articulo 11</h5>
+                <h6>Bogotá Colombia.Diciembre del 2022</h6>
+
+                <img src= "https://previews.123rf.com/images/aprillrain/aprillrain2212/aprillrain221200638/196354278-imagen-de-caricatura-de-un-astronauta-sentado-en-una-luna-ilustraci%C3%B3n-de-alta-calidad.jpg" width="50%"  ></img>
+               
+                <h5>Articulo 12</h5>
+                <h6>Bogotá Colombia.Diciembre del 2022</h6>
+
+                <img src= "https://previews.123rf.com/images/aprillrain/aprillrain2212/aprillrain221200638/196354278-imagen-de-caricatura-de-un-astronauta-sentado-en-una-luna-ilustraci%C3%B3n-de-alta-calidad.jpg" width="50%"  ></img>
+               
+                <h5>Articulo13</h5>
+                <h6>Bogotá Colombia.Diciembre del 2022</h6>
+
+                <img src= "https://previews.123rf.com/images/aprillrain/aprillrain2212/aprillrain221200638/196354278-imagen-de-caricatura-de-un-astronauta-sentado-en-una-luna-ilustraci%C3%B3n-de-alta-calidad.jpg" width="50%"  ></img>
+               
+                <h5>Articulo 14</h5>
+                <h6>Bogotá Colombia.Diciembre del 2022</h6>
+
+                <img src= "https://previews.123rf.com/images/aprillrain/aprillrain2212/aprillrain221200638/196354278-imagen-de-caricatura-de-un-astronauta-sentado-en-una-luna-ilustraci%C3%B3n-de-alta-calidad.jpg" width="50%"  ></img>
+               
+                <h5>Articulo 15</h5>
+                <h6>Bogotá Colombia.Diciembre del 2022</h6>
+
+                <img src= "https://previews.123rf.com/images/aprillrain/aprillrain2212/aprillrain221200638/196354278-imagen-de-caricatura-de-un-astronauta-sentado-en-una-luna-ilustraci%C3%B3n-de-alta-calidad.jpg" width="50%"  ></img>
+               
+                <h5>Articulo 16</h5>
+                <h6>Bogotá Colombia.Diciembre del 2022</h6>
+
+                <img src= "https://previews.123rf.com/images/aprillrain/aprillrain2212/aprillrain221200638/196354278-imagen-de-caricatura-de-un-astronauta-sentado-en-una-luna-ilustraci%C3%B3n-de-alta-calidad.jpg" width="50%"  ></img>
+               
+                <h5>Articulo 17</h5>
+                <h6>Bogotá Colombia.Diciembre del 2022</h6>
+
+                <img src= "https://previews.123rf.com/images/aprillrain/aprillrain2212/aprillrain221200638/196354278-imagen-de-caricatura-de-un-astronauta-sentado-en-una-luna-ilustraci%C3%B3n-de-alta-calidad.jpg" width="50%"  ></img>
+               
+                <h5>Articulo 18</h5>
+                <h6>Bogotá Colombia.Diciembre del 2022</h6>
+
+                <img src= "https://previews.123rf.com/images/aprillrain/aprillrain2212/aprillrain221200638/196354278-imagen-de-caricatura-de-un-astronauta-sentado-en-una-luna-ilustraci%C3%B3n-de-alta-calidad.jpg" width="50%"  ></img>
+               
+                <h5>Articulo 19</h5>
+                <h6>Bogotá Colombia.Diciembre del 2022</h6>
+
+                <img src= "https://previews.123rf.com/images/aprillrain/aprillrain2212/aprillrain221200638/196354278-imagen-de-caricatura-de-un-astronauta-sentado-en-una-luna-ilustraci%C3%B3n-de-alta-calidad.jpg" width="50%"  ></img>
+               
+                <h5>Articulo 20</h5>
+                <h6>Bogotá Colombia.Diciembre del 2022</h6>
+
+                <img src= "https://previews.123rf.com/images/aprillrain/aprillrain2212/aprillrain221200638/196354278-imagen-de-caricatura-de-un-astronauta-sentado-en-una-luna-ilustraci%C3%B3n-de-alta-calidad.jpg" width="50%"  ></img>
+               
+
+              </div>
+
+              <div>
+              <h5>Articulo 21</h5>
+                <h6>Bogotá Colombia.Diciembre del 2022</h6>
+
+                <img src= "https://previews.123rf.com/images/aprillrain/aprillrain2212/aprillrain221200638/196354278-imagen-de-caricatura-de-un-astronauta-sentado-en-una-luna-ilustraci%C3%B3n-de-alta-calidad.jpg" width="50%"  ></img>
+               
+                <h5>Articulo 22</h5>
+                <h6>Bogotá Colombia.Diciembre del 2022</h6>
+
+                <img src= "https://previews.123rf.com/images/aprillrain/aprillrain2212/aprillrain221200638/196354278-imagen-de-caricatura-de-un-astronauta-sentado-en-una-luna-ilustraci%C3%B3n-de-alta-calidad.jpg" width="50%"  ></img>
+               
+                <h5>Articulo 23</h5>
+                <h6>Bogotá Colombia.Diciembre del 2022</h6>
+
+                <img src= "https://previews.123rf.com/images/aprillrain/aprillrain2212/aprillrain221200638/196354278-imagen-de-caricatura-de-un-astronauta-sentado-en-una-luna-ilustraci%C3%B3n-de-alta-calidad.jpg" width="50%"  ></img>
+               
+                <h5>Articulo 24</h5>
+                <h6>Bogotá Colombia.Diciembre del 2022</h6>
+
+                <img src= "https://previews.123rf.com/images/aprillrain/aprillrain2212/aprillrain221200638/196354278-imagen-de-caricatura-de-un-astronauta-sentado-en-una-luna-ilustraci%C3%B3n-de-alta-calidad.jpg" width="50%"  ></img>
+               
+                <h5>Articulo 25</h5>
+                <h6>Bogotá Colombia.Diciembre del 2022</h6>
+
+                <img src= "https://previews.123rf.com/images/aprillrain/aprillrain2212/aprillrain221200638/196354278-imagen-de-caricatura-de-un-astronauta-sentado-en-una-luna-ilustraci%C3%B3n-de-alta-calidad.jpg" width="50%"  ></img>
+               
+                <h5>Articulo 26</h5>
+                <h6>Bogotá Colombia.Diciembre del 2022</h6>
+
+                <img src= "https://previews.123rf.com/images/aprillrain/aprillrain2212/aprillrain221200638/196354278-imagen-de-caricatura-de-un-astronauta-sentado-en-una-luna-ilustraci%C3%B3n-de-alta-calidad.jpg" width="50%"  ></img>
+               
+                <h5>Articulo 27</h5>
+                <h6>Bogotá Colombia.Diciembre del 2022</h6>
+
+                <img src= "https://previews.123rf.com/images/aprillrain/aprillrain2212/aprillrain221200638/196354278-imagen-de-caricatura-de-un-astronauta-sentado-en-una-luna-ilustraci%C3%B3n-de-alta-calidad.jpg" width="50%"  ></img>
+               
+                <h5>Articulo 28</h5>
+                <h6>Bogotá Colombia.Diciembre del 2022</h6>
+
+                <img src= "https://previews.123rf.com/images/aprillrain/aprillrain2212/aprillrain221200638/196354278-imagen-de-caricatura-de-un-astronauta-sentado-en-una-luna-ilustraci%C3%B3n-de-alta-calidad.jpg" width="50%"  ></img>
+               
+                <h5>Articulo 29</h5>
+                <h6>Bogotá Colombia.Diciembre del 2022</h6>
+
+                <img src= "https://previews.123rf.com/images/aprillrain/aprillrain2212/aprillrain221200638/196354278-imagen-de-caricatura-de-un-astronauta-sentado-en-una-luna-ilustraci%C3%B3n-de-alta-calidad.jpg" width="50%"  ></img>
+               
+                <h5>Articulo 30</h5>
+                <h6>Bogotá Colombia.Diciembre del 2022</h6>
+
+                <img src= "https://previews.123rf.com/images/aprillrain/aprillrain2212/aprillrain221200638/196354278-imagen-de-caricatura-de-un-astronauta-sentado-en-una-luna-ilustraci%C3%B3n-de-alta-calidad.jpg" width="50%"  ></img>
+               
+              </div>
+
+            </div>
      </header>
     </div>
   );
